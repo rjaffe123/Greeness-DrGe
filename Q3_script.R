0f83dcdb88d646ec4f77d6539c66b623d48e6b12
rjaffe123 <rachael.jaffe@mail.utoronto.ca>
2020-06-18 12:07:58 -0700

    for Q3

---
+++ Q3_script.R
@@ -0,0 +1,142 @@
+library(raster)
+library(maptools)
+library(spatstat)
+library(dplyr)
+library(sparr)
+library(rgdal)
+
+## FOR TORONTO
+
+
+# tt_total <- read.csv("tt_total.csv") 
+# tt_log <- tt_total %>% mutate(case_control = ifelse(is.na(travel_time),0,1))
+# tt_log<- tt_log %>% mutate(case_control = as.numeric(case_control))
+# tt_log_2005 <- tt_log %>% filter(CDNAME == "Toronto")
+tt_log_2019 <- read.csv("/home/rjaffe/tt_log_2019.csv")
+shp.toronto <- shapefile("/home/rjaffe/toronto/Neighbourhoods.shp")
+lonlat <- cbind(tt_log_2019$longitude, tt_log_2019$latitude)
+sp.case <- SpatialPoints(lonlat,CRS("+proj=longlat +datum=WGS84"))
+newcrs <- proj4string(shp.toronto)
+sp.case <- spTransform(sp.case,	CRS(newcrs))
+
+## Parks
+#driving
+tt_2019_park <- tt_log_2019 %>% filter(year == 2019 & CDNAME == "Toronto" & mode == "driving" & green_type == "park")
+lonlat <- cbind(tt_2019_park$longitude, tt_2019_park$latitude)
+sp.case <- SpatialPoints(lonlat,CRS("+proj=longlat +datum=WGS84"))
+newcrs <- proj4string(shp.toronto)
+sp.case<-spTransform(sp.case, CRS(newcrs))
+spdf.case <- SpatialPointsDataFrame(sp.case,	data=tt_2019_park)
+regions <- slot(shp.toronto, "polygons")
+regions <- lapply(regions, function(x){ SpatialPolygons(list(x))})
+windows <- lapply(regions, as.owin)
+shp.toronto1 <-tess(tiles=windows)
+w.toronto <- as.owin(shp.toronto1)
+xy <- coordinates(spdf.case)
+pp.case <- ppp(xy[, 1], xy[, 2], window = w.toronto, marks = as.factor(spdf.case$case_control))
+ds.case <- density(pp.case)
+spp.case <- split(pp.case)
+pp.access <- spp.case$"1"
+pp.noaccess <- spp.case$"0"
+hpilot.access <- BOOT.density(pp.access)
+hpilot.noaccess <- BOOT.density(pp.noaccess)
+hglobal <- OS(pp.case, nstar = "geometric")
+
+rho.hat <- risk(f = pp.access, g = pp.noaccess, h0 = hglobal, adapt = c(hpilot.access, hpilot.noaccess), tolerate = TRUE)
+
+
+pdf("/home/rjaffe/toronto_park_driving.pdf", width = 10, height = 8)
+plot(rho.hat, tol.args = list(levels = c(0.01, 0.05), lty = 1:2), xlab = "Easting", ylab = "Northing")
+dev.off()
+
+## Forest
+#driving
+tt_2019_forest <- tt_log_2019 %>% filter(year == 2019 & CDNAME == "Toronto" & mode == "driving" & green_type == "forest")
+lonlat <- cbind(tt_2019_forest$longitude, tt_2019_forest$latitude)
+sp.case <- SpatialPoints(lonlat,CRS("+proj=longlat +datum=WGS84"))
+newcrs <- proj4string(shp.toronto)
+sp.case<-spTransform(sp.case, CRS(newcrs))
+spdf.case <- SpatialPointsDataFrame(sp.case,	data=tt_2019_forest)
+regions <- slot(shp.toronto, "polygons")
+regions <- lapply(regions, function(x){ SpatialPolygons(list(x))})
+windows <- lapply(regions, as.owin)
+shp.toronto1 <-tess(tiles=windows)
+w.toronto <- as.owin(shp.toronto1)
+xy <- coordinates(spdf.case)
+pp.case <- ppp(xy[, 1], xy[, 2], window = w.toronto, marks = as.factor(spdf.case$case_control))
+ds.case <- density(pp.case)
+spp.case <- split(pp.case)
+pp.access <- spp.case$"1"
+pp.noaccess <- spp.case$"0"
+hpilot.access <- BOOT.density(pp.access)
+hpilot.noaccess <- BOOT.density(pp.noaccess)
+hglobal <- OS(pp.case, nstar = "geometric")
+
+rho.hat <- risk(f = pp.access, g = pp.noaccess, h0 = hglobal, adapt = c(hpilot.access, hpilot.noaccess), tolerate = TRUE) 
+
+
+pdf("/home/rjaffe/toronto_forest_driving.pdf", width = 10, height = 8)
+plot(rho.hat, tol.args = list(levels = c(0.01, 0.05), lty = 1:2), xlab = "Easting", ylab = "Northing")
+dev.off()
+
+
+##Walking
+#Park
+tt_2019_park <- tt_log_2019 %>% filter(year == 2019 & CDNAME == "Toronto" & mode == "walking" & green_type == "park")
+lonlat <- cbind(tt_2019_park$longitude, tt_2019_park$latitude)
+sp.case <- SpatialPoints(lonlat,CRS("+proj=longlat +datum=WGS84"))
+newcrs <- proj4string(shp.toronto)
+sp.case<-spTransform(sp.case, CRS(newcrs))
+spdf.case <- SpatialPointsDataFrame(sp.case,	data=tt_2019_park)
+regions <- slot(shp.toronto, "polygons")
+regions <- lapply(regions, function(x){ SpatialPolygons(list(x))})
+windows <- lapply(regions, as.owin)
+shp.toronto1 <-tess(tiles=windows)
+w.toronto <- as.owin(shp.toronto1)
+xy <- coordinates(spdf.case)
+pp.case <- ppp(xy[, 1], xy[, 2], window = w.toronto, marks = as.factor(spdf.case$case_control))
+ds.case <- density(pp.case)
+spp.case <- split(pp.case)
+pp.access <- spp.case$"1"
+pp.noaccess <- spp.case$"0"
+hpilot.access <- BOOT.density(pp.access)
+hpilot.noaccess <- BOOT.density(pp.noaccess)
+hglobal <- OS(pp.case, nstar = "geometric")
+
+rho.hat <- risk(f = pp.access, g = pp.noaccess, h0 = hglobal, adapt = c(hpilot.access, hpilot.noaccess), tolerate = TRUE) 
+
+
+pdf("/home/rjaffe/toronto_park_walking.pdf", width = 10, height = 8)
+plot(rho.hat, tol.args = list(levels = c(0.01, 0.05), lty = 1:2), xlab = "Easting", ylab = "Northing")
+dev.off()
+
+
+## Forest
+tt_2019_forest <- tt_log_2019 %>% filter(year == 2019 & CDNAME == "Toronto" & mode == "walking" & green_type == "forest")
+lonlat <- cbind(tt_2019_forest$longitude, tt_2019_forest$latitude)
+sp.case <- SpatialPoints(lonlat,CRS("+proj=longlat +datum=WGS84"))
+newcrs <- proj4string(shp.toronto)
+sp.case<-spTransform(sp.case, CRS(newcrs))
+spdf.case <- SpatialPointsDataFrame(sp.case,	data=tt_2019_forest)
+regions <- slot(shp.toronto, "polygons")
+regions <- lapply(regions, function(x){ SpatialPolygons(list(x))})
+windows <- lapply(regions, as.owin)
+shp.toronto1 <-tess(tiles=windows)
+w.toronto <- as.owin(shp.toronto1)
+xy <- coordinates(spdf.case)
+pp.case <- ppp(xy[, 1], xy[, 2], window = w.toronto, marks = as.factor(spdf.case$case_control))
+ds.case <- density(pp.case)
+spp.case <- split(pp.case)
+pp.access <- spp.case$"1"
+pp.noaccess <- spp.case$"0"
+hpilot.access <- BOOT.density(pp.access)
+hpilot.noaccess <- BOOT.density(pp.noaccess)
+hglobal <- OS(pp.case, nstar = "geometric")
+
+rho.hat <- risk(f = pp.access, g = pp.noaccess, h0 = hglobal, adapt = c(hpilot.access, hpilot.noaccess), tolerate = TRUE) 
+
+
+pdf("/home/rjaffe/toronto_forest_walking.pdf", width = 10, height = 8)
+plot(rho.hat, tol.args = list(levels = c(0.01, 0.05), lty = 1:2), xlab = "Easting", ylab = "Northing")
+dev.off()
+
